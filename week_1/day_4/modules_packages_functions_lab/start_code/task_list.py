da886cb7f0e7125b2b11d06d81051231b889c0a0
Delfland <d_dallison@hotmail.com>
2022-11-10 22:15:57 +0000

    commit day 4 including lab work

--- week_1/day_4/modules_packages_functions_lab/start_code/task_list.py
+++ week_1/day_4/modules_packages_functions_lab/start_code/task_list.py
@@ -10,25 +10,47 @@ tasks = [
 
 ## Get a list of uncompleted tasks
 def get_uncompleted_tasks(list):
-    pass
+    uncompleted_tasks = []
+    for task in list:
+        if task["completed"] == False:
+            uncompleted_tasks.append(task)
+    return uncompleted_tasks
+
+        
+
+
 
 ## Get a list of completed tasks
 def get_completed_tasks(list):
-    pass
+    completed_tasks = []
+    for task in list:
+        if task["completed"] == True:
+            completed_tasks.append(task)
+    return completed_tasks
 
 ## Get tasks where time_taken is at least a given time
 def get_tasks_taking_at_least(list, time):
-    pass
+    tasks_taking_at_least = []
+    for task in list:
+        if task["time_taken"] >= time:
+            tasks_taking_at_least.append(task)
+    return tasks_taking_at_least
 
 ## Find a task with a given description
 def get_task_with_description(list, description):
-    pass
+    for task in list:
+        if task["description"] == description:
+            return task
 
 # Extention (Function): 
 
 ## Get a list of tasks by status
 def get_tasks_by_status(list, status):
-    pass
+    tasks_by_status = []
+    for task in list:
+        if task["completed"] == status:
+            tasks_by_status.append(task)
+    return tasks_by_status
 
 def mark_task_complete(task):
     task["completed"] = True
