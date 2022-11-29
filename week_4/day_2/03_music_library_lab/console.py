2dc8b00750df5595485d1b68412d8ad3ae8b9b7a
Delfland <d_dallison@hotmail.com>
2022-11-29 16:11:38 +0000

    updated with week4/day2 content

---
+++ week_4/day_2/03_music_library_lab/console.py
@@ -0,0 +1,18 @@
+import pdb
+from models.artist import Artist
+from models.album import Album
+import repositories.artist_repository as artist_repository
+import repositories.album_repository as album_repository
+
+artist_repository.delete_all()
+
+artist1 = Artist('James')
+artist_repository.save(artist1)
+artist2 = Artist('Moby')
+artist_repository.save(artist2)
+
+album1 = Album('laid', 'pop', artist1)
+album_repository.save(album1)
+
+
+pdb.set_trace()
---
+++ week_4/day_2/03_music_library_lab/repositories/album_repository.py
@@ -0,0 +1,36 @@
+from db.run_sql import run_sql
+
+import repositories.artist_repository as artist_repository
+from models.album import Album
+
+def select(id):
+    artist = artist_repository.select(result['artist_id'])
+    album = None
+    sql = "SELECT * FROM albums WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+    if results:
+        result = results[0]
+        album = Album(result['title'], result['genre'], artist, result['id'])
+        return album
+
+def select_all():
+    albums = []
+
+    sql = "SELECT * FROM albums"
+    results = run_sql(sql)
+
+    for row in results:
+        artist = artist_repository.select(row['artist_id'])
+        album = Album(row['title'], row['id'])
+        albums.append(album)
+    return albums
+
+def save(album):
+    sql = f'''INSERT INTO albums (title, genre, artist_id)
+    VALUES (%s, %s, %s) RETURNING *'''
+    values = [album.title, album.genre, album.artist.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
